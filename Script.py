ecba9cf30f012522e9ad71147cc71ae35149c283
hariniprakash296 <hariniprakash296@gmail.com>
2022-10-11 23:21:15 +0800

    create table queries

--- Script.py
+++ Script.py
@@ -4,7 +4,32 @@ import sqlite3
 # define connection(to connect to a database) and cursor(to interact with a database), the thing specified in brackets is the database name
 connection = sqlite3.connect('314_database.db')
 
-#create user table
+cursor = connection.cursor()
+
+# create user table
 
 command1 = """CREATE TABLE IF NOT EXISTS
-User(user_id INTEGER PRIMARY KEY, login_email )"""
+User(user_id INTEGER PRIMARY KEY, login_email TEXT, login_pw TEXT, name TEXT, user_type TEXT)"""
+
+cursor.execute(command1)
+
+# create purchases table
+
+command2 = """CREATE TABLE IF NOT EXISTS
+Paper( paper_id INTEGER PRIMARY KEY, array_authors_id INTEGER, paper_name TEXT, paper_details TEXT, array_reviewer_ids INTEGER, reviewer_bid REAL, acceptance_state INTEGER,FOREIGN KEY(array_reviewer_ids) REFERENCES User(user_id), FOREIGN KEY(array_authors_id) REFERENCES User(user_id))"""
+
+cursor.execute(command2)
+
+# create review table
+
+command3 = """CREATE TABLE IF NOT EXISTS
+Review(review_id INTEGER, paper_id INTEGER, reviewer_id INTEGER, review_details TEXT, reviewer_rating INTEGER, author_rating INTEGER, array_comments TEXT, FOREIGN KEY(paper_id) REFERENCES Paper(Paper_id), FOREIGN KEY(reviewer_id) REFERENCES User(user_id))"""
+
+cursor.execute(command3)
+
+# create reviewer table
+
+command4 = """CREATE TABLE IF NOT EXISTS
+Reviewer(user_id INTEGER, max_papers INTEGER, FOREIGN KEY(user_id) REFERENCES User(user_id))"""
+
+cursor.execute(command4)
