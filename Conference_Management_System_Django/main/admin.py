1f7f00c25fc9db77af04c25eb884fbe28ac0e90f
Isaac Lee Jun Ann <51111677+zaealee@users.noreply.github.com>
2022-10-21 12:22:08 +0800

    update from isaac
    replace models.py with my new ones
    update admin.py with the new classes
    added instructions.txt for instructions on how to run server

--- Conference_Management_System_Django/main/admin.py
+++ Conference_Management_System_Django/main/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from main import models
 
-admin.site.register(models.Conference)
-admin.site.register(models.Chair)
-admin.site.register(models.Author)
-admin.site.register(models.Reviewer)
 admin.site.register(models.User)
 admin.site.register(models.Paper)
-admin.site.register(models.Submits)
-admin.site.register(models.Assigns)
+admin.site.register(models.Reviewer)
+admin.site.register(models.Bids)
+admin.site.register(models.Authors)
 admin.site.register(models.Reviews)
+admin.site.register(models.ReviewComments)
 
--- Conference_Management_System_Django/main/models.py
+++ Conference_Management_System_Django/main/models.py
@@ -3,77 +3,57 @@ from django.utils import timezone
 from main import constants
 
 
-
 #entities
 class User(models.Model):
-    email = models.TextField(null=False,primary_key=True)
-    password = models.TextField()
-    type = models.TextField(null=False)
-class Conference(models.Model):
-    name = models.TextField(null=False,primary_key=True)
-    start_date = models.DateTimeField(null=False)
-    end_date = models.DateTimeField(null=False)
-    abstract_submission_deadline = models.DateTimeField(null=False)
-    paper_submission_deadline = models.DateTimeField(null=False)
-    review_deadline = models.DateTimeField(null=False)
-
-class Chair(models.Model):
-    email = models.TextField(null=False,primary_key=True)
-    password = models.TextField()
+    user_id = models.TextField(null=False,primary_key=True)
+    login_email = models.TextField(null=False)
+    login_pw = models.TextField(null=False)
     name = models.TextField(null=False)
-    is_accepted = models.BooleanField(default=False)
-
-class Author(models.Model):
-    email = models.TextField(null=False, primary_key=True)
-    password = models.TextField()
-    name = models.TextField(null=False)
-
-class Reviewer(models.Model):
-    email = models.TextField(null=False, primary_key=True)
-    password = models.TextField()
-    name = models.TextField(null=False)
-    is_accepted = models.BooleanField(default=False)
-
+    is_suspended = models.BooleanField(default=False)
+    user_type = models.TextField(null=False)                    #0 = system admin
+                                                                #1 = conference chair
+                                                                #2 = reviewer
+                                                                #3 = author
+    
 class Paper(models.Model):
-    name = models.TextField(null=False)
-    paper_file_path = models.TextField(null=True)
-    status = models.TextField(default=constants.PAPAERSUBMIT_INITIAL)
-    author = models.ForeignKey('Author',on_delete=models.CASCADE)
-    abstract_file_path = models.TextField(null=False)
-    upload_date = models.DateTimeField(default=timezone.now())
-
+    paper_id = models.TextField(null=False,primary_key=True)
+    paper_name = models.TextField(default="")
+    paper_details = models.TextField(default="")
+    acceptance_state = models.TextField(default=0)              #0 = unsubmitted/pending
+                                                                #1 = unaccepted
+                                                                #2 = accepted
+    
+class Reviewer(models.Model):
+    user_id = models.ForeignKey('User',on_delete=models.CASCADE)
+    max_papers = models.TextField(default=5)
+    
+    
+    
 #relationships
-class Submits(models.Model):
-    conference = models.ForeignKey('Conference', on_delete=models.CASCADE)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    paper = models.ForeignKey('Paper', on_delete=models.CASCADE,unique=True)
-    submit_date = models.DateTimeField(default=timezone.now())
-    submit_status = models.TextField(default=constants.PAPERSUBMIT_ABSTRACT_PENDING)
-    is_scheduled = models.BooleanField(default=False)
+class Bid(model.Model):
+    reviewer_id = models.ForeignKey('User',on_delete=models.CASCADE)
+    paper_id = models.ForeignKey('Paper',on_delete=models.CASCADE)
+    is_bidding = models.BooleanField(default=True)
 
+class Author(model.Model):
+    author_id = models.ForeignKey('User',on_delete=models.CASCADE)
+    paper_id = models.ForeignKey('Paper',on_delete=models.CASCADE)
 
-
-class Creates(models.Model):
-    chair = models.ForeignKey('Chair', on_delete=models.CASCADE)
-    conference = models.ForeignKey('Conference', on_delete=models.CASCADE)
-
-class Assigns(models.Model):
-    paper = models.ForeignKey('Paper', on_delete=models.CASCADE)
-    chair = models.ForeignKey('Chair', on_delete=models.CASCADE)
-    reviewer = models.ForeignKey('Reviewer', on_delete=models.CASCADE)
-    conference = models.ForeignKey('Conference',on_delete=models.CASCADE)
-    class Meta:
-        unique_together = ('paper','reviewer')
-
-class Reviews(models.Model):
-    review_text_abstract = models.TextField(null=True)
-    review_text_paper = models.TextField(null=True)
-    reviewer = models.ForeignKey('Reviewer', on_delete=models.CASCADE)
-    paper = models.ForeignKey('Paper', on_delete=models.CASCADE)
-    class Meta:
-        unique_together = ('paper','reviewer')
-
-class Schedules(models.Model):
-    chair = models.ForeignKey('chair', on_delete=models.CASCADE)
-    paper = models.ForeignKey('Paper', on_delete=models.CASCADE)
-    conference = models.ForeignKey('Conference', on_delete=models.CASCADE)
+class Review(models.Model):
+    review_id = models.TextField(null=False,primary_key=True)
+    paper_id = models.ForeignKey('Paper',on_delete=models.CASCADE)
+    reviewer_id = models.ForeignKey('User',on_delete=models.CASCADE)
+    review_details = models.TextField(null=False)
+    reviewer_rating = models.TextField(null=False)
+    author_rating = models.TextField(null=False)
+    
+class ReviewComment(models.Model):
+    comment_id = models.TextField(null=False,primary_key=True)
+    review_id = models.ForeignKey('Review',on_delete=models.CASCADE)
+    commenter_id = models.ForeignKey('User',on_delete=models.CASCADE)
+    comment_text = models.TextField(null=False)
+    
+    # type = models.TextField(null=False)
+    # end_date = models.DateTimeField(null=False)
+    # is_accepted = models.BooleanField(default=False)
+    # author = models.ForeignKey('Author',on_delete=models.CASCADE)
