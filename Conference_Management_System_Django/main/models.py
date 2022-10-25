f7b6b74cf98734847231b546ea2a67377792a5e9
Isaac Lee Jun Ann <51111677+zaealee@users.noreply.github.com>
2022-10-25 18:07:00 +0800

    edit models to have auto_incrementing primary key, and added all subclasses

--- Conference_Management_System_Django/main/models.py
+++ Conference_Management_System_Django/main/models.py
@@ -6,7 +6,7 @@ from polymorphic.models import PolymorphicModel
 
 #entities
 class User(PolymorphicModel):
-    user_id = models.TextField(null=False, primary_key=True, default=1, unique=True)
+    user_id = models.AutoField(null=False, primary_key=True)
     login_email = models.TextField(null=False)
     login_pw = models.TextField(null=False)
     name = models.TextField(null=False)
@@ -15,17 +15,32 @@ class User(PolymorphicModel):
                                                                 #1 = conference chair
                                                                 #2 = reviewer
                                                                 #3 = author
+class SystemAdmin(User):
+    def __str__(self):
+        return "System Admin"
+
+class ConferenceChair(User):
+    def __str__(self):
+        return "Conference Chair"
 
 class Reviewer(User):
     max_papers = models.TextField(null=False, default=5)
+
+    def __str__(self):
+        return "Reviewer"
+
+class Author(User):
+    def __str__(self):
+        return "Author"
     
 class Paper(models.Model):
-    paper_id = models.TextField(null=False, primary_key=True)
+    paper_id = models.AutoField(null=False, primary_key=True)
     paper_name = models.TextField(default="")
     paper_details = models.TextField(default="")
-    acceptance_state = models.TextField(default=0)              #0 = unsubmitted/pending
-                                                                #1 = unaccepted
-                                                                #2 = accepted
+    acceptance_state = models.TextField(default=constants.PAPERSTATUS_NOTSUBMITTED)             #0 = unsubmitted
+                                                                                                #1 = submitted but pending
+                                                                                                #2 = submitted and unaccepted
+                                                                                                #3 = submitted and accepted
     
     
     
@@ -41,7 +56,7 @@ class Authors(models.Model):
     paper_id = models.ForeignKey('Paper', on_delete=models.CASCADE)
 
 class Reviews(models.Model):
-    review_id = models.TextField(null=False, primary_key=True)
+    review_id = models.AutoField(null=False, primary_key=True)
     paper_id = models.ForeignKey('Paper', on_delete=models.CASCADE)
     reviewer_user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     review_details = models.TextField(null=False)
@@ -49,7 +64,7 @@ class Reviews(models.Model):
     author_rating = models.TextField(null=False)
     
 class ReviewComments(models.Model):
-    comment_id = models.TextField(null=False,primary_key=True)
+    comment_id = models.AutoField(null=False,primary_key=True)
     review_id = models.ForeignKey('Reviews',on_delete=models.CASCADE)
     commenter_user_id = models.ForeignKey('User',on_delete=models.CASCADE)
     comment_text = models.TextField(null=False)
