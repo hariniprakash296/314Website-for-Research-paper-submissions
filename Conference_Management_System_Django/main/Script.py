339b217e69f36484868a56c098c9d2cbd83625fe
hariniprakash296 <hariniprakash296@gmail.com>
2022-10-25 23:38:53 +0800

    Script moved to main and change bground css file

--- Conference_Management_System_Django/main/Script.py
+++ Conference_Management_System_Django/main/Script.py
@@ -1,14 +1,21 @@
 from multiprocessing import connection
 import sqlite3
 
-# define connection(to connect to a database) and cursor(to interact with a database), the thing specified in brackets is the database name
-connection = sqlite3.connect('314_database.db')
+from sqlite3 import Error
 
-cursor = connection.cursor()
+def sql_connection():
+    try:
+        # define connection(to connect to a database) and cursor(to interact with a database), the thing specified in brackets is the database name
+        connection = sqlite3.connect('db.db')
 
-# create user table
+    except Error:
+        print(Error)
 
-command1 = """CREATE TABLE IF NOT EXISTS
+def sql_table(connection):
+        cursorObj = connection.cursor()
+
+        # create user table
+        command1 = """CREATE TABLE IF NOT EXISTS
 User(user_id INTEGER PRIMARY KEY, login_email TEXT, login_pw TEXT, name TEXT, user_type TEXT)"""
 
 cursor.execute(command1)
